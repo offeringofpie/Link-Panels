e286ef83cdb4383fc6bc312e98e75aacd0e22b3c
offeringofpie <offeringofpie@gmail.com>
2016-04-08 11:06:49 +0200

    Added options Syncs options Added reddit, pastebin,improved twitch

--- js/options.js
+++ js/options.js
@@ -4,27 +4,58 @@ var data = document.getElementById('data'),
   height = document.getElementById('height'),
   set = document.getElementById('set');
 
+var sync = {
+  update: function() {
+    chrome.storage.sync.get("options", function(settings) {
+      if (!chrome.runtime.error) {
+        width.value = settings.options.width;
+        width.placeholder = settings.options.width;
+        height.value = settings.options.height;
+        height.placeholder = settings.options.height;
+        if (settings.options.isWide) {
+          wide.checked = true;
+          height.disabled = true;
+        }
+      }
+    });
+  },
+  set: function(object) {
+    chrome.storage.sync.set({
+      "options": object
+    }, function() {
+      if (chrome.runtime.error) {
+        console.log("Runtime error.");
+      }
+    });
+  }
+};
+width.onchange = function() {
+  if (wide.checked) {
+    height.value = Math.floor(width.value * 9 / 16);
+  }
+};
+//width.value = 450;
 wide.onchange = function() {
-  data.innerText += "text";
+  if (wide.checked) {
+    height.disabled = true;
+  } else {
+    height.disabled = false;
+    height.value = Math.floor(width.value * 9 / 16);
+  }
 };
 
 document.body.onload = function() {
-  chrome.storage.sync.get("data", function(items) {
-    if (!chrome.runtime.error) {
-      console.log(items);
-      document.getElementById("data").innerText = items.data;
-    }
-  });
+  sync.update();
 };
 
 document.getElementById("set").onclick = function() {
-  var d = width.value;
-  chrome.storage.sync.set({
-    "data": d
-  }, function() {
-    if (chrome.runtime.error) {
-      console.log("Runtime error.");
-    }
+  var w = parseInt(width.value),
+  h = parseInt(height.value),
+  isWide = wide.checked;
+  sync.set({
+    width: w,
+    height: h,
+    isWide: isWide
   });
   window.close();
 };
--- js/parse.js
+++ js/parse.js
@@ -3,7 +3,10 @@ var p = {
   pats: {
     dailymotion: /dailymotion.com[\w\/#]+video[\/=]([a-z0-9]+)/,
     instagram: /instagr(?:\.am|am\.com)\/p\/([a-zA-Z0-9]+)/,
+    pastebin: /pastebin\.com\/(.+)/,
+    reddit: /reddit\.com\/(.+)?/,
     soundcloud: /soundcloud\.com\//,
+    twitch: /twitch\.tv\/([a-zA-z0-9]+)(?:\/v\/)?(\d+)?/,
     vimeo: /vimeo\.com\/(?:[a-z]+\/)?(?:[a-z]+\/)?(?:[a-z]+\/)?(\w+)/,
     vine: /vine\.co\/v\/(.+)/,
     youtube: /youtu(?:\.)?be(?:\.com)?\/(?:watch\?v=|embed\/|v\/)?([a-zA-Z0-9\_\-]+)/
@@ -16,8 +19,19 @@ var p = {
     instagram: function(url, match) {
       return 'https://www.instagram.com/p/' + match[1] + '/embed/';
     },
+    pastebin: function(url, match) {
+      return 'https://pastebin.com/embed_iframe.php?i=' + match[1];
+    },
+    reddit: function(url, match) {
+      var permalink = (match[1] === undefined) ? '' : match[1];
+      return 'https://m.reddit.com/' + permalink;
+    },
     soundcloud: function(url, match) {
       return 'https://w.soundcloud.com/player/?url=' + encodeURIComponent(url) + '&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&visual=true';
+    },
+    twitch: function(url, match) {
+      var video = (match[2] === undefined) ? '&video=v' + match[2] : '';
+      return 'https://player.twitch.tv/?channel=' + match[1] + video;
     },
     vimeo: function(url, match) {
       return 'https://player.vimeo.com/video/' + match[1];

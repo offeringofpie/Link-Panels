53ae0285069c50c470f00614cf7ada60aa8b2df7
J Lopes <offeringofpie@gmail.com>
2016-07-29 08:35:00 +0200

    - improved regex

--- js/parse.js
+++ js/parse.js
@@ -2,19 +2,24 @@ var p = {
   /* regex patterns */
   pats: {
     dailymotion: /dailymotion.com[\w\/#]+video[\/=]([a-z0-9]+)/,
+    gist: /gist\.github\.com\/([-0-9a-zA-Z]+\/)?([0-9a-fA-f]+)/,
     instagram: /instagr(?:\.am|am\.com)\/p\/([a-zA-Z0-9]+)/,
     pastebin: /pastebin\.com\/(.+)/,
     reddit: /reddit\.com\/(.+)?/,
     soundcloud: /soundcloud\.com\//,
+    tedtalks: /ted\.com\/talks\/(.*)/,
     twitch: /twitch\.tv\/([a-zA-z0-9]+)(?:\/v\/)?(\d+)?/,
     vimeo: /vimeo\.com\/(?:[a-z]+\/)?(?:[a-z]+\/)?(?:[a-z]+\/)?(\w+)/,
     vine: /vine\.co\/v\/(.+)/,
-    youtube: /youtu(?:\.)?be(?:\.com)?\/(?:watch\?v=|embed\/|v\/)?([a-zA-Z0-9\_\-]+)/
+    youtube: /youtu(?:\.)?be(?:\.com)?\/(?:watch\?v=|embed\/|v\/)?([a-zA-Z0-9\_\-]+)(?:&list=)?([a-zA-Z0-9\_\-]+)?/
   },
   /* embed links */
   embed: {
     dailymotion: function(url, match) {
       return 'http://www.dailymotion.com/embed/video/' + match[1] + '?api=postMessage&html=1';
+    },
+    gist: function(url, match) {
+      return 'data:text/html, <title>' + url + '</title><script  src="https://gist.github.com/' + match[1] + '/' + match[2] + '.js"></script>';
     },
     instagram: function(url, match) {
       return 'https://www.instagram.com/p/' + match[1] + '/embed/';
@@ -29,9 +34,13 @@ var p = {
     soundcloud: function(url, match) {
       return 'https://w.soundcloud.com/player/?url=' + encodeURIComponent(url) + '&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&visual=true';
     },
+    tedtalks: function(url, match) {
+      return 'https://embed-ssl.ted.com/talks/' + match[1] + ".html";
+    },
     twitch: function(url, match) {
-      var video = (match[2] === undefined) ? '&video=v' + match[2] : '';
-      return 'https://player.twitch.tv/?channel=' + match[1] + video;
+      var param = (match[2] === undefined) ? '?channel=' : '?video=v',
+          which = (match[2] === undefined) ? 1 : 2;
+      return 'https://player.twitch.tv/' + param + match[which];
     },
     vimeo: function(url, match) {
       return 'https://player.vimeo.com/video/' + match[1];
@@ -40,7 +49,8 @@ var p = {
       return 'https://vine.co/v/' + match[1] + '/embed/simple';
     },
     youtube: function(url, match) {
-      return 'http://www.youtube.com/embed/' + match[1];
+      var playlist = (match[2] === undefined) ? '' : '?list=' + match[2];
+      return 'http://www.youtube.com/embed/' + match[1] + playlist;
     }
   }
 };

cd0f67e98bfb8bc1496a8fcc1342d24dbf1200e7
J Lopes <offeringofpie@gmail.com>
2017-04-24 21:37:35 +0200

    * url pattern tests
    * corrected reddit regex

--- src/js/background.js
+++ src/js/background.js
@@ -1,1 +1,12 @@
 import './parts/panel.js'
+import sync from './parts/sync';
+
+sync.get((options) => {
+  if (typeof options.length === "undefined") {
+    sync.set({
+      width: 500,
+      height: 281,
+      isWide: true
+    });
+  }
+});

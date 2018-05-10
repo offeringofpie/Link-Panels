9c72d7bd828e22629eb229fde0fce98d6d7b285f
J Lopes <offeringofpie@gmail.com>
2018-05-10 17:06:37 +0200

    options update

--- js/options.js
+++ js/options.js
@@ -1,12 +1,12 @@
 import sync from './parts/sync.js';
 
 let optionsElems = {
-  data: document.getElementById('data'),
-  wide: document.getElementById('wide'),
-  width: document.getElementById('width'),
-  height: document.getElementById('height'),
-  experimental: document.getElementById('experimental'),
-  set: document.getElementById('set')
+  data: document.querySelector('#data'),
+  wide: document.querySelector('#wide'),
+  width: document.querySelector('#width'),
+  height: document.querySelector('#height'),
+  experimental: document.querySelector('#experimental'),
+  set: document.querySelector('#set')
 };
 
 optionsElems.width.onchange = () => {
@@ -25,21 +25,33 @@ optionsElems.wide.onchange = () => {
 };
 
 document.body.onload = () => {
-  sync.update((options) => {
-    optionsElems.wide.checked = options.isWide;
-    optionsElems.experimental.checked = options.experimental;
-    optionsElems.width.value = options.width;
-    optionsElems.height.value = options.height;
-    optionsElems.height.value = options.height;
+  chrome.storage.sync.get('options', opts => {
+    if (!chrome.runtime.lastError) {
+      optionsElems.wide.checked = opts.checked;
+      optionsElems.experimental.checked = opts.experimental;
+      optionsElems.width.value = opts.width;
+      optionsElems.height.value = opts.height;
+    } else {
+      console.error(chrome.runtime.lastError);
+    }
   });
 };
 
 optionsElems.set.onclick = () => {
-  sync.set({
-    width: parseInt(optionsElems.width.value),
-    height: parseInt(optionsElems.height.value),
-    isWide: optionsElems.wide.checked,
-    experimental: optionsElems.experimental.checked
-  });
+  chrome.storage.sync.set(
+    {
+      options: {
+        width: parseInt(optionsElems.width.value),
+        height: parseInt(optionsElems.height.value),
+        isWide: optionsElems.wide.checked,
+        experimental: optionsElems.experimental.checked
+      }
+    },
+    () => {
+      if (chrome.runtime.lastError) {
+        console.log('Runtime error.');
+      }
+    }
+  );
   window.close();
 };
